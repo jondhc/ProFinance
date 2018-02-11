7f7b06e0fd1ec253441bf6bcb093ca5228837d97
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2018-02-10 18:54:03 -0600

    Initial commit

---
+++ src/View.java
@@ -0,0 +1,8 @@
+/**
+ * Created by gerardoayala on 3/3/16.
+ */
+public interface View
+{
+    public void construyeComponentes();
+    public void setActionListener(Controller theController);
+}//end View
